90723e34c156ca88bf257976b23dec14fc5d2263
Jeet Mondal <jm382118@gmail.com>
2026-03-13 12:04:36 +0530

    fix duplicate db connection

--- init/index.js
+++ init/index.js
@@ -1,25 +1,53 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/AIRBNB";
+
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () => {
+//   // await Listing.deleteMany({});
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
+// initDB();
+
+
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// Atlas connection string
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/AIRBNB";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const dbUrl =
+  "mongodb+srv://jm382118_db_user:Jeet2004JMMJ1904@cluster0.mp9zztg.mongodb.net/airbnb?retryWrites=true&w=majority";
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+  console.log("Connected to Atlas DB");
 }
 
 const initDB = async () => {
-  // await Listing.deleteMany({});
+  await Listing.deleteMany({});
   await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+  console.log("Data was initialized");
 };
 
-initDB();
+main()
+  .then(() => initDB())
+  .catch((err) => console.log(err));
